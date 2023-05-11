d99bb43a5e54febcbe8b38973c54ca1d878a76e9
Bushra-Almusallami <130510018+Bushra-Almusallami@users.noreply.github.com>
2023-05-11 14:52:15 +0400

    Day5

---
+++ problems.py
@@ -0,0 +1,9 @@
+s=("|rx#duh#juhdwh")
+for i in s:
+        n=(chr(ord(i)-3))
+        print(n, end="")
+#############################
+number=int(input("Enter the number:"))
+sum=0
+for i in number:
+    
