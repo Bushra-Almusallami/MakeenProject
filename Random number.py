028653113353dfd2167f4157284bc1047fb9306d
Bushra-Almusallami <130510018+Bushra-Almusallami@users.noreply.github.com>
2023-05-14 17:23:18 +0400

    Day6

---
+++ Random number.py
@@ -0,0 +1,12 @@
+message = ' 345-876   '
+
+# remove leading and trailing whitespaces
+print(message.('-',''))
+#####################################
+r=[1,2,3,4]
+for x in r:
+    if x == 3:
+        break
+    print(x)
+
+#############################
