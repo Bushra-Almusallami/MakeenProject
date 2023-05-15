e79b2cc3585b3548922854bbdcc05df71c402030
Bushra-Almusallami <130510018+Bushra-Almusallami@users.noreply.github.com>
2023-05-15 17:36:23 +0400

    Day7

---
+++ List without range function.py
@@ -0,0 +1,21 @@
+l=[22,3,-2,-1,10,1]
+for i in l:
+    print(i,end=' ')
+#######################
+"""l=[22,3,-2,-1,10,1]
+count=0
+for i in l:
+    if i<0:
+        count=count+1
+print(count)"""
+
+#########################
+"""l=[22,3,-2,-1,10,1]
+count=0
+m=1
+for i in l:
+    m= m*i
+    count=count+1
+print(m)"""
+
+
